f5ba73860509ee47b22d405fe9f95171b53b312c
Parminder Klair <perminder.klair@gmail.com>
2018-12-04 12:27:15 +0530

    ui fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import styled from 'styled-components/native';
 
 import Card from './components/Card';
 
 const Wrapper = styled.View`
   background-color: #ffffff;
-  margin-top:50px;
+  margin-top: 50;
 `;
 
 const Columns = styled.View`
   display: flex;
   flex-direction: row;
-  margin: 4px;
-`;
-
-const Column = styled.View`
-  flex-basis: 0;
-  flex-grow: 1;
-  flex-shrink: 1;
-  border: 1px solid grey;
-  margin: 4px;
+  margin-vertical: 8;
+  margin-horizontal: 8;
 `;
 
 export default class App extends Component {
@@ -57,17 +50,32 @@ export default class App extends Component {
               <Card />
             </Columns>
           </ScrollView>
-          <Columns>
-            <Column>
-              <Text>bcjbdckjsd</Text>
-            </Column>
-            <Column>
-              <Text>bcjbdckjsd</Text>
-            </Column>
-            <Column>
-              <Text>bcjbdckjsd</Text>
-            </Column>
-          </Columns>
+          <ScrollView horizontal>
+            <Columns>
+              <Card />
+            </Columns>
+            <Columns>
+              <Card />
+            </Columns>
+            <Columns>
+              <Card />
+            </Columns>
+            <Columns>
+              <Card />
+            </Columns>
+            <Columns>
+              <Card />
+            </Columns>
+            <Columns>
+              <Card />
+            </Columns>
+            <Columns>
+              <Card />
+            </Columns>
+            <Columns>
+              <Card />
+            </Columns>
+          </ScrollView>
         </Wrapper>
       </ScrollView>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,9 +6,10 @@ const TVEventHandler = require('TVEventHandler');
 
 const StyledView = styled.View`
   background-color: ${props=> props.active ? 'red' : 'papayawhip' };
-  padding: 5px;
-  width: 200px;
-  height: ${props=> props.active ? '100px' : 'auto' };
+  padding-vertical: 5;
+  padding-horizontal: 5;
+  width: 200;
+  height: 200
 `;
 
 class Card extends Component {
@@ -18,21 +19,17 @@ class Card extends Component {
       active:false
     }
   }
-  
- onFocus = () => {
-   this.setState({active:!this.state.active});
- }
-render() {
+
+  onFocus = () => {
+    this.setState({active:!this.state.active});
+  }
+
+  render() {
     return (
       <TouchableWithoutFeedback  onFocus={this.onFocus} onLongPress={this.onFocus} onBlur={this.onFocus}>
         <StyledView active={this.state.active}>
           <Text>
-            hello
-            hxvhcxvcz
-            xhcvhxvcx 
-            cvhxvchxzhvch 
-            cxjvchxzvhcvh 
-            cvhxvchvx
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </Text>
         </StyledView>
       </TouchableWithoutFeedback>
