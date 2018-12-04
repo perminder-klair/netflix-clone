1efe2c20cd1ff483814d3e53c0850f89ecc8e081
Parminder Klair <perminder.klair@gmail.com>
2018-12-04 12:33:47 +0530

    added tv handler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import styled from 'styled-components/native';
+var TVEventHandler = require('TVEventHandler');
 
 import Card from './components/Card';
 
@@ -17,6 +18,29 @@ const Columns = styled.View`
 `;
 
 export default class App extends Component {
+  _tvEventHandler;
+
+  enableTVEventHandler() {
+    this._tvEventHandler = new TVEventHandler();
+    this._tvEventHandler.enable(this, function(cmp, evt) {
+      if (evt && evt.eventType === 'right') {
+        cmp.setState({board: cmp.state.board.move(2)});
+      } else if(evt && evt.eventType === 'up') {
+        cmp.setState({board: cmp.state.board.move(1)});
+      } else if(evt && evt.eventType === 'left') {
+        cmp.setState({board: cmp.state.board.move(0)});
+      } else if(evt && evt.eventType === 'down') {
+        cmp.setState({board: cmp.state.board.move(3)});
+      } else if(evt && evt.eventType === 'playPause') {
+        cmp.restartGame();
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.enableTVEventHandler();
+  }
+
   render() {
     return (
       <ScrollView>
